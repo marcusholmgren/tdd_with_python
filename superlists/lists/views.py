772c3343a487140711d909783e836580526850af
Marcus Holmgren <marcus.holmgren1@gmail.com>
2016-06-25 20:37:48 +0200

    Create list application and a first view unit test.

---
+++ superlists/lists/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+class home_page():
+    pass
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'lists.views.home_page', name='Home'),
     url(r'^admin/', admin.site.urls),
 ]
