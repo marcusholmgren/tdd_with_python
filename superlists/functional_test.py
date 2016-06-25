a7a2e9a93986c33ec25fe995b07b30980682be77
Marcus Holmgren <marcus.holmgren1@gmail.com>
2016-06-25 18:02:42 +0200

    Functional test now for Firefox with unit test harness.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Safari()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert "Django" in browser.title
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.set_page_load_timeout(30)
+    
+
+    def tearDown(self):
+        self.browser.quite()
+
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        self.browser.get("http://127.0.0.1:8000/")
+
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
