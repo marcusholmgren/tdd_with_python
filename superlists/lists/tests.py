0d09017bf2c03508b4abcf12e63e4cd4130b8bca
Marcus Holmgren <marcus.holmgren1@gmail.com>
2016-07-09 22:05:40 +0200

    Model for list Items and associated migration.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
     
@@ -27,3 +28,25 @@ class HomePageTest(TestCase):
         self.assertIn("A new list item", response.content.decode())
         expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
         self.assertEqual(response.content.decode()[0:80], expected_html[0:80])
+
+
+class ItemModelTest(TestCase):
+
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2, "Expected 2 items in collection.")
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the second")
+
