de3ca8ff942d5d8659b3983edf89d9d4379eb941
Marcus Holmgren <marcus.holmgren1@gmail.com>
2016-08-24 21:46:21 +0200

    POST and redirect added

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,12 +5,11 @@ from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item
 
+
 class HomePageTest(TestCase):
-    
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
-
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
@@ -23,7 +22,6 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0, "Expcted items to be empty on initial request.")
 
-
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = "POST"
@@ -35,7 +33,6 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
-
     def test_home_page_redirects_after_POST(self):
         request = HttpRequest()
         request.method = "POST"
@@ -44,23 +41,11 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302, "Expected status code 302")
-        self.assertEqual(response['location'], '/', "Expected home page")
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/',
+                         "/lists/the-only-list-in-the-world/")
 
 
 class ItemModelTest(TestCase):
-
-
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = "The first (ever) list item"
@@ -78,3 +63,13 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
 
+
+class ListViewTest(TestCase):
+    def test_display_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
